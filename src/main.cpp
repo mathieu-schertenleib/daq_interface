94d511b7053416a3aeccd13b4b5c82066eec2456
mathieu-schertenleib <schertenleib.mathieu@gmail.com>
2023-04-22 12:24:50 +0200

    Cleanup CMakeLists.txt

--- src/main.cpp
+++ src/main.cpp
@@ -8,10 +8,6 @@ int main()
 {
     try
     {
-        log_info("This is an info message {}\n", 0);
-        log_warning("This is a warning message {}\n", 1);
-        log_error("This is an error message {}\n", 2);
-
         Application app;
         app.run();
     }
